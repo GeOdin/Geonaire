ca88dc2036537907b4dfe1fb47ec8a6c184b8419
GeOdin <madeleine.vanwinkel@wur.nl>
2016-11-24 20:12:34 +0100

    Added index.html, css\stylesheet.css, Geonaire.html, css\Geonaire.css and js\Geonaire.js
    Added
    * index.html
    * css\stylesheet.css
    * Geonaire.html
    * css\Geonaire.css
    * js\Geonaire.js

---
+++ js/Geonaire.js
@@ -0,0 +1,123 @@
+/* Geonaire.js
+ * JavaScript file voor de Geonaire
+ * Gemaakt op 2016-11-24
+ * Het laatst veranderd op 2016-11-24
+ * door GeOdin
+ *
+ *===================================
+ * Dingen die gedaan moeten worden: *
+ *===================================
+ * Algemeen
+ * index.html
+ * stylesheet.css
+ * * momenteel geen dingen die nog gedaan moeten worden
+ * Geonaire.html
+ * Geonaire.css
+ * * momenteel geen dingen die nog gedaan moeten worden
+ * Geonaire.js
+ * * momenteel geen dingen die nog gedaan moeten worden
+ *
+ *========================================
+ * Functies binnen deze JavaScript file: *
+ *========================================
+ * 
+ *==========================================
+ * Variabelen binnen deze JavaScript file: *
+ *==========================================
+ * 
+ */
+
+//////////////
+// Algemeen //
+//////////////
+
+/*
+ ============
+ * Algemeen *
+ ============
+ * Zorg dat alles in het Nederlands is voor de doelgroep
+ * Doelgroep: 18+ Nederlanders
+ * U of jij voor de vragen?
+ * Website online zetten
+ * Link maken met database
+ * * Database van de universiteit gebruiken (vraag Ron) of eventueel een gratis online database gebruiken (met de kans dat de data op een gegeven moment niet bruikbaar meer is), eventueel in combinatie met een mail sturen naar een te maken emailadres
+ * Haal de informatie die ik bedacht heb uit m'n mail en stop het georganiseerd hier
+ * * Zet (M) (S) (C) (W) voor de dingen die ik bedacht heb, conform met het MoSCoW-schema (M van must/ S van should/ C van would/ W van Would)
+ ================
+ * Achievements *
+ ================
+ * Mogelijkheid om achievements te verdienen
+ * Progress counter maken om de speler te laten zien hoeveel van de hoeveel achievements hij heeft?
+ * Mogelijke achievements:
+ * * De doorzetter/ wachter (met een timer tellen hoelang een speler over de vragenlijst doet en na een bepaalde tijd deze achievement geven)
+ ===========
+ * Buttons *
+ ===========
+ * Knoppen om verschillende functies/ features te toggelen
+ ============
+ * Features *
+ ============
+ * Entertainment
+ * * cartoons/ comics per vraag
+ * Easter eggs
+ * * spelletjes/ puzzels per vraag
+ * * speler krijgt een toepasselijke token en achievement voor elke opgeloste puzzel/ spel
+ ===========
+ * Markers *
+ ===========
+ ==========
+ * Player *
+ ==========
+ * Titel - mogelijkheid dat de speler een titel krijg conform de dingen die hij gedaan heeft
+ =============
+ * Questions *
+ =============
+ * Nog een keer goed nadenken of de vragen goed gesteld zijn en niet op meerdere manieren interpretabel
+ * THEMA: USER INFO
+ * 1. Wat is uw leeftijd? (moet een geheel getal, hoger dan 0 zijn)
+ * 2. Wat is uw nationaliteit? (drop-down list)
+ * 3. Op welke postcode woont u? (inzoomen naar postcode gebied)
+ * THEMA: GAMEN
+ * 4. Hoeveel tijd speelt u over het algemeen computerspellen? (vul een getal in en kies de toepasbare tijdsaanduiding) (x uren per dag/week/maand/jaar)
+ * 5. Wat voor spellen speelt u zoal? (zie IGN voor genres, geef voorbeelden en voeg de optie augmented reality games toe met de voorbeelden Pokémon Go en Ingress)
+ * THEMA: LOCATION
+ * 6. Als u het over uw omgeving hebt, tot hoever reikt deze? (optie geven om met tools een cirkel (en eventueel ook een vierkant/ polygon) te tekenen (evt opslaan als GeoJSON))
+ * 7. Stel de gemeente zou een windmolen (of eventueel opsplitsen tot 2 vragen, 1 voor positief en 1 voor negatieve verandering) willen plaatsen, tot hoe dichtbij uw huis zou u dit goed vinden? (eventueel een marker van een windmolen laten plaatsen, of weer een cirkel/vierkant/polygon laten maken (evt opslaan als GeoJSON))
+ ==========
+ * Tokens *
+ ==========
+ * Speler krijgt een token voor elke beantwoorde vraag
+ * Tokens zijn zichtbaar in #player
+ */
+
+////////////////
+// index.html //
+////////////////
+
+/*
+ * Een kleine introductie geven over het hoe en wat van de vragenlijst (wat ik bovenaan de vragenlijst zou zetten als ik hem op zou sturen)
+ * Eventueel een favicon toevoegen als icoontje voor de webpagina
+ * * <link rel="shortcut icon" type="image/png" href="favicon.png" />
+ */
+
+////////////////////
+// stylesheet.css //
+////////////////////
+
+///////////////////
+// Geonaire.html //
+///////////////////
+
+/*
+ * Een kleine introductie geven over hoe de vragenlijst ingevuld moet worden en een teaser om mensen op eventuele extra functies/features te wijzen.
+ * Eventueel een favicon toevoegen als icoontje voor de webpagina
+ * * <link rel="shortcut icon" type="image/png" href="favicon.png" />
+ */
+
+//////////////////
+// Geonaire.css //
+//////////////////
+
+/////////////////
+// Geonaire.js //
+/////////////////
